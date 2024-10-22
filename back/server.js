eb738f1d14f43c9939d9609bd711f927c21b00f8
SaraMartinez21 <a21sarmarbau@inspedralbes.cat>
2024-10-22 12:57:19 +0200

    el node recibe datos desde mysql

--- back/server.js
+++ back/server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const mysql = require('mysql2');
 const { spawn } = require('child_process'); 
 const app = express();
 const port = 3000;
-const mysql = require('mysql2');
+
 
 const db = mysql.createConnection({
-    host: 'http://daw.inspedralbes.cat/phpmyadmin/',  // IP o dominio del servidor MySQL
-    user: 'a21sarmarbau_alcohol',       // Usuario de la base de datos
-    password: 'Alcohol1',  // Contraseña de la base de datos
-    database: 'a21sarmarbau_alcohol' // Nombre de la base de datos
+    host: 'localhost',
+    user: 'root', 
+    password: '',
+    database: 'tr1',
+    connectTimeout: 10000
   });
 
-  app.get('/', (req, res) => {
+  app.get('/api/productos', (req, res) => {
     db.query('SELECT * FROM productos', (err, result) => {
         if (err) {
             return res.status(500).send('Error en la consulta a la base de datos')
@@ -27,18 +29,11 @@ const db = mysql.createConnection({
         console.error('Error', err);
         return;
     }
-    console.log('Error', results);
+    console.log('Si', results);
   });
   db.end();
 
-  app.get('/api/productos', (req, res) => {
-    fs.readFile(dataFilePath, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).send('Error reading the file');
-        }
-        res.json(JSON.parse(data));
-    });
-});
+
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
