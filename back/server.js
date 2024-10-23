96e51c8107093e5bc11cd28ae2a4d9b528b8b22a
SaraMartinez21 <a21sarmarbau@inspedralbes.cat>
2024-10-23 18:47:59 +0200

    arreglar el select

--- back/server.js
+++ back/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mysql = require('mysql2');
-const fs = require('fs'); // Para leer el archivo JSON
+const fs = require('fs');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 // const port = 23457;
 
 // Middleware para permitir el parsing de JSON en los requests
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -35,9 +37,9 @@ db.connect((err) => {
     const createTableQuery = `
         CREATE TABLE IF NOT EXISTS productos (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            image_url VARCHAR(255) NOT NULL,
-            price DECIMAL(10, 2) NOT NULL
+            producto VARCHAR(255) NOT NULL,
+            imagen VARCHAR(255) NOT NULL,
+            precio DECIMAL(10, 2) NOT NULL
         )`;
 
     db.query(createTableQuery, (err) => {
@@ -57,26 +59,26 @@ db.connect((err) => {
 
             // Insertar los productos en la base de datos
             productos.forEach(producto => {
-                const checkQuery = 'SELECT * FROM productos WHERE name = ?';
-                db.query(checkQuery, [producto.name], (err, result) => {
+                const checkQuery = 'SELECT * FROM productos WHERE producto = ?';
+                db.query(checkQuery, [producto.producto], (err, result) => {
                     if (err) {
                         console.error('Error en la consulta:', err);
                         return;
                     }
                     if (result.length > 0) {
-                        console.log('El producto ya existe:', producto.name);
+                        console.log('El producto ya existe:', producto.producto);
                         return;
                     }
                     if(result.length == 0 ){
-                        const insertQuery = 'INSERT INTO productos (name, image_url, price) VALUES (?, ?, ?)';
-                        db.query(insertQuery, [producto.name, producto.image_url, producto.price], (err) => {
+                        const insertQuery = 'INSERT INTO productos (producto, imagen, precio) VALUES (?, ?, ?)';
+                        db.query(insertQuery, [producto.producto, producto.imagen, producto.precio], (err) => {
                             if (err) {
                                 console.error('Error al insertar el producto:', err);
                             }
                         });
                         console.log('Productos insertados en la tabla "productos".');
                     }else{
-                        console.log('El producto ya existe:', producto.name);
+                        console.log('El producto ya existe:', producto.producto);
                     }
                 });         
             });
@@ -86,7 +88,7 @@ db.connect((err) => {
 });
 
 // Obtener todos los productos
-app.get('/api/productos', (req, res) => {
+app.get('/getProducto', (req, res) => {
     const query = 'SELECT * FROM productos'; 
     
     db.query(query, (err, result) => {
