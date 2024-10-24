2f4302583e5bcf132cd827f0b2691d3f0b052d10
BernatMir <a24bermirpre@inspedralbes.cat>
2024-10-24 12:58:15 +0200

    vuetifyCreacion

---
+++ racoglop/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="producto in productos"
+        :key="producto.id"
+        cols="12"
+        md="4"
+      >
+        <v-card>
+          <v-img
+            :src="producto.imagen"
+            alt="Imagen de producto"
+            height="200px"
+          ></v-img>
+          <v-card-title>{{ producto.nombre }}</v-card-title>
+          <v-card-subtitle>{{ producto.volumen }} - {{ producto.tipo }}</v-card-subtitle>
+          <v-card-text>
+            {{ producto.descripcion }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+
+<script>
+import productosData from './productos.json'
+
+
+export default {
+  data() {
+    return {
+      productos: productosData.productos
+    }
+  }
+}
+</script>
+
+
