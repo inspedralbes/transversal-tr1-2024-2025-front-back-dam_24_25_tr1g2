4d1e195b19fd8d76411b06cc16011e67fd0b5c96
SaraMartinez21 <123445716+SaraMartinez21@users.noreply.github.com>
2024-10-24 16:14:31 +0200

    crear las pesañas de gestioProductes y cambiar el app.vue

--- racoglop/src/App.vue
+++ racoglop/src/App.vue
@@ -1,42 +1,11 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col
-        v-for="producto in productos"
-        :key="producto.id"
-        cols="12"
-        md="4"
-      >
-        <v-card>
-          <v-img
-            :src="producto.imagen"
-            alt="Imagen de producto"
-            height="200px"
-          ></v-img>
-          <v-card-title>{{ producto.nombre }}</v-card-title>
-          <v-card-subtitle>{{ producto.volumen }} - {{ producto.tipo }}</v-card-subtitle>
-          <v-card-text>
-            {{ producto.descripcion }}
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-
-<script>
-import { getProducto } from '../src/Service/ComunicationManager'
-
-
-onMounted(async () => {
-  productos.value = await getProducto();
-});
-
-export default {
-
-
-}
+<script setup>
+  //
 </script>
-
-
