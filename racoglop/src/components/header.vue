fd419f3455bc6c70033bbca0f0a0e25dd696f190
a23hashusra <a23hashusraf@inspedralbes.cat>
2024-10-29 08:36:28 +0100

    header

---
+++ racoglop/src/components/header.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <header>
+      <v-btn @click="rutaProductos">Gestió de productes</v-btn>
+      <v-btn @click="rutaComandas">Gestió de Comandas</v-btn>
+    </header>
+    <div>
+      <h1>a</h1>
+    </div>
+  </div>
+  
+</template>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const rutaProductos = () => {
+    router.push('/gestioProductes');
+};  
+
+const rutaComandas = () => { 
+  router.push('/gestioComandas');
+}
+
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  gap: 1rem;
+  padding: 1rem;
+  background-color: #f5f5f5; /* Puedes personalizar el color aquí */
+  border-bottom: 1px solid #ddd; /* Añade una línea divisoria si deseas */
+}
+</style>
--- racoglop/src/pages/index.vue
+++ racoglop/src/pages/index.vue
@@ -1,25 +1,8 @@
 <template>
-  <div>
-    <h1>a</h1>
-    
-    <v-btn @click="rutaProductos">Gestió de productes</v-btn>
-    <v-btn @click="rutaComandas">Gestió de Comandas</v-btn>
-
-  </div>
+  <Header />
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
-
-
-const router = useRouter();
-
-const rutaProductos = () => {
-    router.push('/gestioProductes');
-};  
-
-const rutaComandas = () => { 
-  router.push('/gestioComandas');
-}
-
+import header from '../components/header';
 </script>
